ebd6ad8a8f62592759867d3fc905422b6652f68e
Dex Melton <spinning.pc@gmail.com>
2023-08-30 11:00:37 -0400

    basic setup

---
+++ BugTracker/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BugTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BugTracker/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BugTracker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
